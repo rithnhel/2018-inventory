f171945e4e133c2fac5a049c2904bc616ff58ea4
Dalin-LOEM <dalinloem.198@gmail.com>
2018-04-30 10:02:27 +0700

    [IN006] design department layout
    refs IN006

--- application/views/departments/index.php
+++ application/views/departments/index.php
@@ -19,18 +19,18 @@
 <table id="users" cellpadding="0" cellspacing="0" class="table table-striped table-bordered" width="100%">
     <thead>
         <tr>
-            <th>ID</th>
+            <th class="text-right">ID</th>
             <th>Departments</th>
         </tr>
     </thead>
     <tbody>
 <?php foreach ($users as $user):?>
     <tr>
-        <td data-order="<?php echo $user['id']; ?>" data-id="<?php echo $user['id'];?>">
-            <?php echo $user['id'] ?>&nbsp;
+        <td data-order="<?php echo $user['id']; ?>" data-id="<?php echo $user['id'];?>"  class="text-right">
             <!-- <a href="<?php echo base_url();?>users/edit/<?php echo $user['id'] ?>" title="Edit user"><i class="mdi mdi-pencil"></i></a> -->
-            <a href="#" class="confirm-edit" title="Delete user"><i class="mdi mdi-pencil"></i></a>
+            <a href="#" class="confirm-edit" title="edit user"><i class="mdi mdi-pencil"></i></a>
             <a href="#" class="confirm-delete" title="Delete user"><i class="mdi mdi-delete"></i></a>
+            <?php echo $user['id'] ?>&nbsp;
         </td>
         <td><!-- <?php echo $user['firstname']; ?> -->Wood</td>
     </tr>
@@ -39,44 +39,42 @@
         </table>
     </div>
 </div>
-
   <div class="row-fluid"><div class="col-12">&nbsp;</div></div>
-
+<!-- create new department -->
   <div class="row-fluid">
       <div class="col-12">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><i class="mdi mdi-plus"></i>&nbsp;Create new department</button>
-
-                <!-- The Modal -->
-                <div class="modal fade" id="myModal">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-
-                        <!-- Modal Header -->
-                        <div class="modal-header">
-                          <h4 class="modal-title">Create new department</h4>
-                          <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      </div>
-                      
-                      <!-- Modal body -->
-                      <div class="modal-body">
-                        <div class="form-inline">
-                          <label class="control-label" for="firstname">Department</label>
-                          <input type="text" class="form-control" name="firstname" id="firstname" required />
-                      </div>
-                  </div>
-
-                  <!-- Modal footer -->
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                  </div>
-      </div>
+       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+           <i class="mdi mdi-plus-circle"></i>&nbsp;Create new department
+         </button>
+         <!-- The Modal -->
+         <div class="modal fade" id="myModal">
+           <div class="modal-dialog modal-dialog-centered">
+             <div class="modal-content">
+               <!-- Modal Header -->
+               <div class="modal-header">
+                 <h4 class="modal-title">Create new department</h4>
+                 <button type="button" class="close" data-dismiss="modal">&times;</button>
+               </div>          
+               <!-- Modal body -->
+               <div class="modal-body ">
+                <div class="form-inline">
+                      <label class="control-label" for="firstname">Department</label>&nbsp;
+                    <input type="text" class="form-control" name="firstname" id="firstname" required />
+                </div>  
+               </div> 
+               <!-- Modal footer -->
+               <div class="modal-footer">
+                 <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                 <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+               </div>
+             </div>
+           </div>
+         </div>
   </div>
-
 </div>
-
+<!-- delete  -->
 <div id="frmConfirmDelete" class="modal hide fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
         <div class="modal-header">
                 <h5 class="modal-title">Confirmation</h5>
@@ -85,19 +83,18 @@
                 </button>
         </div>
         <div class="modal-body">
-            <!-- <p>You are going to delete a department.</p> -->
             <p>Are you sure that you want to delete this department?</p>
         </div>
         <div class="modal-footer">
-            <a href="#" class="btn btn-danger" id="lnkDeleteUser">Yes</a>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+            <a href="#" class="btn btn-primary" id="lnkDeleteUser">Yes</a>
+            <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
         </div>
         </div>
     </div>
 </div>
 <!-- edit -->
 <div id="frmConfirmEdit" class="modal hide fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
         <div class="modal-header">
                 <h5 class="modal-title">Edit Department</h5>
@@ -106,11 +103,13 @@
                 </button>
         </div>
         <div class="modal-body">
-            <input type="text" class="form-control">
+            <div class="form-inline">
+                <label for="">Department</label> &nbsp;<input type="text" class="form-control">
+            </div>
         </div>
         <div class="modal-footer">
-            <a href="#" class="btn btn-danger" id="lnkDeleteUser">Yes</a>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+            <a href="#" class="btn btn-primary" id="lnkDeleteUser">OK</a>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
         </div>
         </div>
     </div>
